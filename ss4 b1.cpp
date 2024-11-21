84a19f7a62b7efca881fc0e58925abcde874a6a7
hongky06 <dohongky2006@gmail.com>
2024-11-21 22:21:58 +0700

    Add files via upload

---
+++ ss4 b1.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (){
+	int integer;
+	printf ("hay nhap so nguyen: "); 
+	scanf ("%d",&integer);
+	 if(integer <= 0){
+	 	printf ("la so am ") ;
+	 } else {
+	 	printf ("la so duong "); 
+	 } 
+	
+	return 0; 
+} 
