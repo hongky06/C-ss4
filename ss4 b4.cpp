06cc24ae214081c761635345ba0ae7ff634b8add
hongky06 <dohongky2006@gmail.com>
2024-11-22 00:49:25 +0700

    Add files via upload

---
+++ ss4 b4.cpp
@@ -0,0 +1,59 @@
+#include<stdio.h>
+int main(){
+
+
+int month;
+printf ("moi ban nhap thang ");
+scanf("%d",&month);
+ 
+  
+   switch(month) {
+   	
+   	
+   	case 1:
+	   printf ("tháng co 31 ngày ") ;
+	   break; 
+    case 2:
+	   printf ("tháng co 28 hoac 29 ngày ") ;
+	   break;
+	case 3:
+	   printf ("tháng co 31 ngày ") ;
+	   break;   
+    case 4:
+	   printf ("tháng co 30 ngày ") ;
+	   break;
+	case 5:
+	   printf ("tháng co 31 ngày ") ;
+	   break;
+	case 6:
+	   printf ("tháng co 30 ngày ") ;
+	   break;
+	case 7:
+	   printf ("tháng co 31 ngày ") ;
+	   break;
+	case 8:
+	   printf ("tháng co 31 ngày ") ;
+	   break;
+	case 9:
+	   printf ("tháng co 30 ngày ") ;
+	   break;               
+    case 10:
+	   printf ("tháng co 31 ngày ") ;
+	   break;
+    case 11:
+	   printf ("tháng co 30 ngày ") ;
+	   break; 
+    case 12:
+	   printf ("tháng co 31 ngày ") ;
+	   break;
+    default :
+	printf ("loi cu phap , nhap lai ");
+	 
+  
+  
+}
+            return 0; 
+  
+}
+   
+   
