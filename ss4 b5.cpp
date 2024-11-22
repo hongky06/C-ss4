55f6f1cdbb1282b8669934b80ecff941ba671ed5
hongky06 <dohongky2006@gmail.com>
2024-11-22 11:21:19 +0700

    Add files via upload

---
+++ ss4 b5.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main(){
+	int num1,num2,num3;
+	printf("hay nap so tu nhien 1 : ");
+	scanf("%d",&num1);
+	printf("hay nap so tu nhien 2 : ");
+	scanf("%d",&num2);
+	printf("hay nap so tu nhien 3 : ");
+	scanf("%d",&num3);
+	if(num1 < num3 && num3 < num2 ){
+		printf ("co nam trong khoang ");
+	}else {
+		printf ("khong nam trong khoang ");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
